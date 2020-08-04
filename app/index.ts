dc11a520cfd7693745ea2a2348f2c48a562b453a
dev <domemacintosh@gmail.com>
2020-08-04 17:11:08 +0700

    📦 NEW: types

---
+++ app/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { ICheck, IConnect } from './interfaces'
+
+export namespace geeesy {
+  export type ResConnect = IConnect;
+  export type ResCheck = ICheck;
+}
---
+++ app/interfaces.ts
@@ -0,0 +1,9 @@
+export interface IConnect {
+  statusCode: number;
+  message: string;
+}
+
+export interface ICheck {
+  statusCode: number;
+  data: string;
+}
