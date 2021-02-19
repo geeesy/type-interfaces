2877607d2cc6e458da62967d36f573a6d03242a1
Dome J <domemacintosh@gmail.com>
2021-02-20 01:05:23 +0700

    update company cover

--- app/type-company.ts
+++ app/type-company.ts
@@ -105,6 +105,7 @@ export interface ICompanyAsset {
   companyPrimaryColor: string;
   companySecondaryColor: string;
   companyBannersUrl: string[];
+  companyCoverUrl: string;
 }
 
 export interface ICompanySetting {
