d0ff4198a4fe4eaa05b75fda4227fb18945bf403
Dome J <domemacintosh@gmail.com>
2021-10-06 17:58:17 +0700

    fix: order status with null

--- app/type-order.ts
+++ app/type-order.ts
@@ -112,7 +112,7 @@ export interface IOrderEntity {
 export interface IDBOrderStatus {
   statusOrderOnSeller: StatusOrderSeller;
   statusOrderOnBuyer: StatusOrderCustomer;
-  statusOrder: StatusOrder;
+  statusOrder: StatusOrder | null;
 }
 
 
