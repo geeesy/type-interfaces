412c0646b3ec364f86a1fab966ab13e3f032a822
cjd1985 <domemacintosh@gmail.com>
2022-02-09 13:50:02 +0700

    refactor(order): no option on hasnoshipment

--- app/type-order.ts
+++ app/type-order.ts
@@ -202,7 +202,7 @@ export interface IOrderLinkData {
   paymentAvailableIds: string[]; // * USE ID FOR LATER
   shippingAvailableData: GappSetting.ShippingMethod[];
   paymentAvailableData: GappSetting.PaymentMethod[];
-  hasNoShipment?: boolean;
+  hasNoShipment: boolean;
 }
 
 // ANCHOR: ORDER | Entity (9/10)
