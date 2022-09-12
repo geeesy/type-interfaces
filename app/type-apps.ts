0851b6bc2ce48335da54bafe4a6ae299b1b499f2
abg74123 <bisza_i.d@hotmail.com>
2022-09-12 11:22:47 +0700

    change name again

--- app/type-apps.ts
+++ app/type-apps.ts
@@ -1551,7 +1551,7 @@ export interface ICreateNotifyMessage {
   actorFullName: string;
   priority: TNotifyPriority;
   entityId: string;
-  title: string;
+  entityTitle: string;
   entitySubId: string;
   status: TNotifyStatus;
   refDocNo: string;
