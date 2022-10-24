8456f741e3df4d0fd7d4fddfd5a423b1969729e3
abg74123 <bisza_i.d@hotmail.com>
2022-10-24 14:13:20 +0700

    fix(type-facebook):update camel case v.2

--- app/type-facebook.ts
+++ app/type-facebook.ts
@@ -4,7 +4,7 @@ import { IProductRow } from './type-apps';
 export type TBroadcastStatus = 'UNPUBLISHED' | 'LIVE' | 'LIVE_STOPPED' | 'PROCESSING' | 'VOD' | 'SCHEDULED_UNPUBLISHED' | 'SCHEDULED_LIVE' | 'SCHEDULED_EXPIRED' | 'SCHEDULED_CANCELED'
 
 export interface IFacebookLive {
-  FacebookLiveName: string;
+  facebookLiveName: string;
   facebookData: IFacebookData;
   broadcastStatus: TBroadcastStatus;
   liveTimestamp: number;
@@ -12,13 +12,13 @@ export interface IFacebookLive {
 }
 
 export interface IFacebookPost {
-  FacebookPostName: string;
+  facebookPostName: string;
   statusPost: string;
   facebookData: IFacebookData;
   postId: string;
   products: IProductFacebookRow[];
-  periodDateStart: Date;
-  periodDateEnd: Date;
+  periodStartDate: Date;
+  periodEndDate: Date;
 }
 
 export interface IProductFacebookRow extends IProductRow {
