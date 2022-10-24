9ef8027cf46749b39436401b2edd58fef2291ac8
abg74123 <bisza_i.d@hotmail.com>
2022-10-24 15:47:27 +0700

    fix(type-facebook):facebook id

--- app/type-facebook.ts
+++ app/type-facebook.ts
@@ -1,9 +1,19 @@
 import { IFacebookData } from './type-setting';
 import { IProductRow } from './type-apps';
 
-export type TBroadcastStatus = 'UNPUBLISHED' | 'LIVE' | 'LIVE_STOPPED' | 'PROCESSING' | 'VOD' | 'SCHEDULED_UNPUBLISHED' | 'SCHEDULED_LIVE' | 'SCHEDULED_EXPIRED' | 'SCHEDULED_CANCELED'
+export type TBroadcastStatus =
+  | 'UNPUBLISHED'
+  | 'LIVE'
+  | 'LIVE_STOPPED'
+  | 'PROCESSING'
+  | 'VOD'
+  | 'SCHEDULED_UNPUBLISHED'
+  | 'SCHEDULED_LIVE'
+  | 'SCHEDULED_EXPIRED'
+  | 'SCHEDULED_CANCELED';
 
 export interface IFacebookLive {
+  facebookLiveId: string;
   facebookLiveName: string;
   facebookData: IFacebookData;
   broadcastStatus: TBroadcastStatus;
@@ -12,10 +22,11 @@ export interface IFacebookLive {
 }
 
 export interface IFacebookPost {
+  facebookPostId: string;
   facebookPostName: string;
   statusPost: string;
   facebookData: IFacebookData;
-  postId: string;
+  postSelectedId: string;
   products: IProductFacebookRow[];
   periodStartDate: Date;
   periodEndDate: Date;
