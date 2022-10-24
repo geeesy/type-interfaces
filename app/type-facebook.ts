e31dc2b5b9c2e7389efb5a4ace39a3bc3812e139
abg74123 <bisza_i.d@hotmail.com>
2022-10-24 13:34:14 +0700

    fix(type-facebook):add new type

---
+++ app/type-facebook.ts
@@ -0,0 +1,26 @@
+import { IFacebookData } from './type-setting';
+import { IProductRow } from './type-apps';
+
+export type TBroadcastStatus = 'UNPUBLISHED' | 'LIVE' | 'LIVE_STOPPED' | 'PROCESSING' | 'VOD' | 'SCHEDULED_UNPUBLISHED' | 'SCHEDULED_LIVE' | 'SCHEDULED_EXPIRED' | 'SCHEDULED_CANCELED'
+
+export interface IFacebookLive {
+  FacebookLiveName: string;
+  facebook: IFacebookData;
+  broadcastStatus: TBroadcastStatus;
+  liveTimestamp: number;
+  products: IProductFacebookRow[];
+}
+
+export interface IFacebookPost {
+  FacebookPostName: string;
+  statusPost: string;
+  facebook: IFacebookData;
+  postId: string;
+  products: IProductFacebookRow[];
+  periodDateStart: Date;
+  periodDateEnd: Date;
+}
+
+export interface IProductFacebookRow extends IProductRow {
+  codeCf: string;
+}
