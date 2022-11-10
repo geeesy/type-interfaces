cbca7fe4252161c369956edc9a269a4cfeeaed5a
Ratthasart <ratthasat241998@gmail.com>
2022-11-10 14:56:00 +0700

    Overlay OBS

--- app/type-facebook.ts
+++ app/type-facebook.ts
@@ -1,7 +1,7 @@
 import { IFacebookData } from './type-setting';
-import {IOrderProductRow} from "./type-order";
-import {GappSetting} from "./index";
-
+import { IOrderProductRow } from './type-order';
+import { GappSetting } from './index';
+import { IPaymentMethodEntity } from './type-payment';
 
 export type TStatusBroadcast =
   | 'UNPUBLISHED'
@@ -14,8 +14,17 @@ export type TStatusBroadcast =
   | 'SCHEDULED_EXPIRED'
   | 'SCHEDULED_CANCELED';
 
-
 export type TTypeFacebook = 'POST' | 'LIVE';
+
+export type TTypeOverlay =
+  | 'LOGO'
+  | 'BANK'
+  | 'TEXT'
+  | 'SOUND'
+  | 'TIME'
+  | 'PRODUCT';
+
+export type TTypeTemplate = 'DESKTOP' | 'MOBILE';
 
 /**
  * FACEBOOK LIVE
@@ -28,9 +37,10 @@ export interface IFacebookLiveCreate {
   timestampLive: number;
   shippingAvailableIds: string[]; // * USE ID FOR LATER
   paymentAvailableIds: string[]; // * USE ID FOR LATER
-  shippingAvailableData: GappSetting.ShippingMethod[]
+  shippingAvailableData: GappSetting.ShippingMethod[];
   paymentAvailableData: GappSetting.PaymentMethod[];
   hasNoShipment: boolean;
+  templateOBSId: string; // * OBS OverlayID
 }
 
 export interface IFacebookLiveId {
@@ -52,6 +62,11 @@ export interface IFacebookPostCreate {
   statusPost: string;
   periodStartDate: Date;
   periodEndDate: Date;
+  shippingAvailableIds: string[]; // * USE ID FOR LATER
+  paymentAvailableIds: string[]; // * USE ID FOR LATER
+  shippingAvailableData: GappSetting.ShippingMethod[];
+  paymentAvailableData: GappSetting.PaymentMethod[];
+  hasNoShipment: boolean;
 }
 
 export interface IFacebookPostId {
@@ -74,11 +89,11 @@ export interface ICountFacebookPostRow {
  * FACEBOOK PRODUCT ROW
  */
 
-export interface ICreateFacebookProductRow extends IFacebookConnect{
+export interface ICreateFacebookProductRow extends IFacebookConnect {
   products: IProductFacebookRow[];
 }
 
-export interface IFacebookConnect{
+export interface IFacebookConnect {
   type: TTypeFacebook;
   facebookRelationId: string;
 }
@@ -89,7 +104,118 @@ export interface IFacebookConnect{
 
 export interface IProductFacebookRow extends IOrderProductRow {
   codeCf: string;
-  totalReservedStock: number
-  productType: string
-  useInventory: string
+  totalReservedStock: number;
+  productType: string;
+  useInventory: string;
+}
+
+/**
+ * OBS
+ */
+
+// ^ OBS OVERLAY TEMPLATE
+
+export interface IOBSTemplateCreate {
+  templateOBSName: string;
+  templateType: TTypeTemplate;
+}
+
+export interface IOBSTemplateList {
+  templateOBSId: string;
+  templateOBSName: string;
+  templateType: TTypeTemplate;
+}
+
+export interface IOBSTemplateUpdate {
+  templateOBSId: string;
+  templateOBSName: string;
+}
+
+export interface IOBSTemplateDetail {
+  templateOBSId: string;
+  templateOBSName: string;
+  templateType: TTypeTemplate;
+}
+
+export interface IOBSTemplateRow {
+  overlayName: string;
+  templateOBSId: string;
+  overlayType: TTypeOverlay;
+  overlayAttributes:
+    | IOverlayLogo
+    | IOverlayBank
+    | IOverlayText
+    | IOverlaySound
+    | IOverlayTime
+    | IOverlayProduct;
+}
+
+export interface IOBSId {
+  overlayId: string;
+}
+
+export interface IOBSTemplateRowUpdate {
+  overlayId: string;
+  overlayName: string;
+  overlayAttributes:
+    | IOverlayLogo
+    | IOverlayBank
+    | IOverlayText
+    | IOverlaySound
+    | IOverlayTime
+    | IOverlayProduct;
+}
+
+// $ Overlay Attributes
+
+export interface IOverlayLogo {
+  overlayName: string;
+  overlayUrl: string;
+  overlayImage: string;
+}
+
+export interface IOverlayTime {
+  overlayIsCountDown: boolean;
+  overlayUrl: string;
+  overlayTime: ICountDown | ISetTime;
+}
+
+export interface ICountDown {
+  overlayDays: string;
+  overlayHours: string;
+  overlayMinutes: string;
+  overlaySeconds: string;
+}
+
+export interface ISetTime {
+  overlayStartTime: string;
+  overlayEndTime: string;
+}
+
+export interface IOverlayText {
+  overlayUrl: string;
+  overlayDescription: string;
+  overlayTextStyle: string;
+  overlayTextBackgroundColor: string;
+}
+
+export interface IOverlayBank {
+  overlayUrl: string;
+  overlayPaymentData: IPaymentMethodEntity;
+}
+
+export interface IOverlaySound {
+  overlayName: string;
+  overlayUrl: string;
+  overlayCodeSound: string;
+  overlayNote: string;
+}
+
+export interface IOverlayProduct {
+  overlayProducts: IProductFacebookRow[];
+  overlayUrl: string;
+  overlayColorPrimary: string;
+  overlayColorCode: string;
+  overlayColorProductName: string;
+  overlayColorPrice: string;
 }
