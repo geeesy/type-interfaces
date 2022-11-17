952e34bb0fe1154646445152acda67b3f19b0e6e
Ratthasart <ratthasat241998@gmail.com>
2022-11-17 10:59:04 +0700

    Facebook

--- app/type-facebook.ts
+++ app/type-facebook.ts
@@ -243,6 +243,7 @@ export interface IOverlayText {
   overlayDescription: string;
   overlayTextStyle: string;
   overlayTextBackgroundColor: string;
+  overlayIsShowBackgroundColor: string;
 }
 
 export interface IOverlayBank {
