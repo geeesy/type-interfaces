9e1b47da2ccb8853625f12395b77577a83af9e83
Ratthasart <ratthasat241998@gmail.com>
2023-01-19 15:35:10 +0700

    fix:facebook

--- app/type-apps.ts
+++ app/type-apps.ts
@@ -14,6 +14,7 @@ import { StatusApprovalFlow, StatusDocSender, StatusQuotationSender } from './en
 import { IAddress, ICompanyContactInfo } from './type-company';
 import { IOrderLinkDataOnConfirm, IOrderLinkImmuDataOnConfirm } from './type-order';
 import {ITagAttr} from "./type-facebook";
+import {ISaleChannelEntity} from "./type-setting";
 
 export interface IUpdateDocStatus {
   existingDocStatus: StatusDocSender
@@ -167,6 +168,7 @@ export type TVendorType = 'seller' | 'buyer' | 'multi' | 'other';
 
 export interface IVendorFromFacebook {
   facebookUserId: string;
+  facebookPages: ISaleChannelEntity[]
   tags: ITagAttr[],
   isBlocked: boolean
 }
