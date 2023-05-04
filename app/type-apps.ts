fad827a1bb9b661161945328aa670112260bb2d5
Ratthasart <ratthasat241998@gmail.com>
2023-05-04 11:34:26 +0700

    update

--- app/type-apps.ts
+++ app/type-apps.ts
@@ -294,6 +294,11 @@ export interface ISenderContact {
   senderCompanyContactInfo: ICompanyContactInfo;
 }
 
+export interface IDeliveryContact {
+  deliveryContactInfo: IPersonContactInfo;
+  deliveryCompanyContactInfo: ICompanyContactInfo;
+}
+
 export interface IReceiverContact {
   receiverContactInfo: IPersonContactInfo;
   receiverCompanyContactInfo: ICompanyContactInfo;
@@ -615,6 +620,7 @@ export interface IQuotationFormCreate {
   formGroupAccounting: IQuotationFormGroupAccounting;
   formGroupData: IQuotationFormGroupData;
   formGroupApprover: IQuotationFormGroupApprover;
+  formGroupDelivery: IDeliveryContact;
 }
 
 export interface IQuotationRfqFormCreate {
